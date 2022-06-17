b72448f3ba6c2ccf24110c32610079d991454e1c
ProBra <lopesmoshe@hotmail.com>
2022-06-17 22:07:04 +0300

    Project First Commit After Deleting Api Key

---
+++ SourceCode/config.js
@@ -0,0 +1,14 @@
+const projectConfig = {
+	db: {
+		url: "mongodb://localhost:27017/",
+		name: "wefix"
+	},
+	port: 3000,
+	jwtSecret: "wefixproject12312322",
+	email: {
+		username: "WeFix.Garages.Management@gmail.com",
+		password: "WeFixRafaOr2020!!@#"
+	}
+};
+
+module.exports = projectConfig;
